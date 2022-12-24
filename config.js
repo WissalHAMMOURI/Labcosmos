6a33605703afba3076de71f95abcead2515430fb
WissalHAMMOURI <ELHAMMOURI_WISSAL@emsi-edu.ma>
2022-12-24 15:44:37 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {};
+config.host = process.env.HOST || "https://wissalelhammouri.documents.azure.com:443/";
+config.authKey =
+process.env.AUTH_KEY || "WWOXp2FF7VCRIcygIqCfOWXRSmHaLdGE0I3BCD3n67jmsMEKDnifZ4TfzA0eixSL0jKXlEJzEKKuACDbMZbfiA==";
+config.databaseId = "ToDoList";
+config.containerId = "Items";
+if (config.host.includes("https://localhost:")) {
+ console.log("Local environment detected");
+ console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
+ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+ console.log(`Go to http://localhost:${process.env.PORT || '3000'} to
+try the sample.`);
+}
+module.exports = config;
